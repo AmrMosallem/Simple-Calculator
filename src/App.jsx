789c72d82a3b69157f289f6755acc8b7fb93548a
Amr Mosallem <amromsllam@gmail.com>
2025-07-03 00:43:59 +0300

    refactor: update button implementation and enhance styling; remove unused Button component

--- src/App.jsx
+++ src/App.jsx
@@ -7,24 +7,74 @@ function App() {
   // Use State variables
   const [expression, setExpression] = useState("");
   const [isCalculated, setIsCalculated] = useState(false);
-  const [btnTxt, setBtnTxt] = useState([
-    { text: "1", value: "1" },
-    { text: "2", value: "2" },
-    { text: "3", value: "3" },
-    { text: "+", value: "+" },
-    { text: "4", value: "4" },
-    { text: "5", value: "5" },
-    { text: "6", value: "6" },
-    { text: "-", value: "-" },
+  const btnTxt = [
+    { text: "C", value: "C", className: "col-span-2 bg-blue-600" },
+    { text: "/", value: "/", className: "bg-blue-600" },
+    { text: "*", value: "*", className: "bg-blue-600" },
     { text: "7", value: "7" },
     { text: "8", value: "8" },
     { text: "9", value: "9" },
-    { text: "*", value: "*" },
-    { text: "=", value: "=" },
-    { text: "C", value: "C" },
-    { text: "0", value: "0" },
-    { text: "/", value: "/" },
-  ]);
+    { text: "-", value: "-", className: "bg-blue-600" },
+    { text: "4", value: "4" },
+    { text: "5", value: "5" },
+    { text: "6", value: "6" },
+    { text: "+", value: "+", className: "bg-blue-600" },
+    { text: "1", value: "1" },
+    { text: "2", value: "2" },
+    { text: "3", value: "3" },
+    { text: "=", value: "=", className: "row-span-2" },
+    { text: "0", value: "0", className: "col-span-2" },
+    { text: ".", value: "." },
+  ]
+
+  function handleClick(event) {
+    {
+      const value = event.target.value;
+      if (value === "C") {
+        setExpression("");
+        setIsCalculated(false);
+        return;
+      }
+
+      if (value === "=") {
+        try {
+          const result = evaluate(expression);
+          if (!isFinite(result)) {
+            setExpression("Error: Division by zero");
+            setIsCalculated(true);
+            return;
+          } else {
+            setExpression(result.toString());
+            setIsCalculated(true);
+          }
+        } catch (error) {
+          setExpression("Error" + error.message);
+        }
+      } else {
+        if (isCalculated) {
+          if (
+            value === "+" ||
+            value === "-" ||
+            value === "*" ||
+            value === "/"
+          ) {
+            setExpression((prev) => prev + value);
+          } else {
+            setExpression(value);
+          }
+          setIsCalculated(false);
+        } else {
+          if (value === "C") {
+            setExpression("");
+            setIsCalculated(false);
+            return;
+          } else {
+            setExpression((prev) => prev + value);
+          }
+        }
+      }
+    }
+  }
 
   return (
     <>
@@ -42,62 +92,22 @@ function App() {
         {/* Buttons*/}
         <div className="grid grid-cols-4 grid-rows-4 gap-2 w-full flex-1">
           {btnTxt.map((btn) => (
-            <Button
-              text={btn.text}
+            <button
               value={btn.value}
-              onClick={(event) => {
-                const value = event.target.value;
-                if (value === "C") {
-                  setExpression("");
-                  setIsCalculated(false);
-                  return;
-                }
-
-                if (value === "=") {
-                  try {
-                    const result = evaluate(expression);
-                    if (!isFinite(result)) {
-                      setExpression("Error: Division by zero");
-                      setIsCalculated(true);
-                      return;
-                    } else {
-                      setExpression(result.toString());
-                      setIsCalculated(true);
-                    }
-                  } catch (error) {
-                    setExpression("Error" + error.message);
-                  }
-                } else {
-                  if (isCalculated) {
-                    if (
-                      value === "+" ||
-                      value === "-" ||
-                      value === "*" ||
-                      value === "/"
-                    ) {
-                      setExpression((prev) => prev + value);
-                    } else {
-                      setExpression(value);
-                    }
-                    setIsCalculated(false);
-                  } else {
-                    if (value === "C") {
-                      setExpression("");
-                      setIsCalculated(false);
-                      return;
-                    } else {
-                      setExpression((prev) => prev + value);
-                    }
-                  }
-                }
-              }}
-            />
+              onClick={handleClick}
+              className={`font-['Abhaya_Libre'] font-bold flex items-center justify-center text-2xl font-semibold font-semibold py-2 rounded border border-transparent focus:outline-none focus:ring-2 focus:ring-sky-300 transition
+                        ${btn.text === "="
+                  ? "bg-sky-300 text-slate-900 hover:bg-sky-400"
+                  : "bg-slate-600 text-sky-300 hover:bg-slate-700 hover:border-sky-300"
+                } ${btn.className}`}
+            >{btn.text}
+            </button>
           ))}
         </div>
       </div>
 
       {/* Footer */}
-      <footer className="footer mt-4 text-center text-sm text-slate-800 text-lg">
+      <footer className="footer font-['Tangerine'] mt-4 text-center text-white text-lg">
         <p>
           Made with <span className="text-red-700"> &hearts;</span> <br /> Zeinab
           Hassan
